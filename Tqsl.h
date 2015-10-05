f7ce5d54c55ea4468e9662200e71a53f6688246f
Andrew Madsen <andrew@mixedinkey.com>
2015-10-05 15:25:13 -0600

    Renamed product to Tqsl.framework. Added real master/umbrella header.

---
+++ Tqsl.h
@@ -0,0 +1,15 @@
+//
+//  Tqsl.h
+//  Tqsl
+//
+//  Created by Andrew Madsen on 10/10/15.
+//
+//
+
+#import <Tqsl/tqsllib.h>
+#import <Tqsl/adif.h>
+#import <Tqsl/cabrillo.h>
+#import <Tqsl/location.h>
+#import <Tqsl/openssl_cert.h>
+#import <Tqsl/tqslconvert.h>
+#import <Tqsl/tqslerrno.h>
